90b636eed9a90d48a20afcb14347ebb2c2a820bb
Ronald Holden <rholden.rh@gmail.com>
2021-07-14 22:04:24 -0500

    Files for the first project of The 404VeggieBusters

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,50 @@
-// variable for ...
-// variable for ...
-// variable for ...
-// variable for ...
+console.log("hi");
+// variable for cities
+var cityLocation = document.getElementById("cityLocation");
+
+// variable for search
+var searchButton = document.getElementById("searchButton");
+
+// variable for search results 
+// variable for ALL the API keys that we use
 
 
-// add event listener for 
-// add event listener for 
+// create function called getCity to grab local restaurants in city that is searched (below)
+
+// function getCity(event) {
+//     event.preventDefault();
+//     create variable of city we're searching 
+//     var citySearch = cities.value;
+//     console.log(citySearch);
+//     consult api about city's plant based restaurants 
+//     getCoordinates(citySearch); 
+// pass city name into it. parameter with city 
+// };
+
+// function to get coordinates of location/places around it 
+
+// function to display search results 
+
+// use parse to get data from websites to get only what we need (lesson 6 - activity 7)
+
+// look at lesson 6 - activity 23
+
+// make sure to set parameters in function, whether it's an event, response, etc. 
+
+// hover effect on images of recipes / search results
+
+// fetch APIs
+
+
+// create local storage for different pages to be stored
+
+
+// add event listener for search button
+searchButton.addEventListener("click", function() {
+    
+});
+
+// add event listener for moving from one webpage to another
+// add event listener clicking on images and recipes
+
 // media query (bottom of page)
