e0183f3c891cdf2a6e7126041ae55f35c164c7d8
valbona1992 <valbona12@gmail.com>
2021-07-17 00:24:07 -0500

    Add code for API recipe function

--- assets/recipes_script.js
+++ assets/recipes_script.js
@@ -4,13 +4,15 @@
 $("#searchButton").on("click", function(event){
     event.preventDefault();
     let searchBox = $("#search-box").val();
+    let diet = $('input[name="diet"]:checked').val();
 
-    callSearchAPI(searchBox, "vegan");
+    callSpoonacularSearchAPI(searchBox, diet);
+
 
 });
 
 
-function callSearchAPI(searchBox, diet){
+function callSpoonacularSearchAPI(searchBox, diet){
     let key = "2eb98eaed78c4117ae15b00c1b061474";
 
     let queryURL = "https://api.spoonacular.com/recipes/complexSearch?query=" + searchBox +"&diet=" + diet + "&number=9&apiKey=" + key;
@@ -26,5 +28,10 @@ function callSearchAPI(searchBox, diet){
     .then(function (data) {
         // Check if the response is 200
         console.log(data);
+        let boxSelector = $(".box");
+        for (let i = 0; i < boxSelector.length; i++){
+            $(boxSelector[i]).html(data["results"][i]["title"]);
+            $(boxSelector[i]).append("<img src='" + data["results"][i]["image"] +"'>")
+        }
     }); 
 }
