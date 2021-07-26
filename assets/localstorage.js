cc65f40931ef763943f7ff2e2c8ef6fa3a31bbef
valbona1992 <valbona12@gmail.com>
2021-07-26 17:26:33 -0500

    removed some commented out code

--- assets/localstorage.js
+++ assets/localstorage.js
@@ -8,9 +8,6 @@ console.log("Loading Local Storage", favRestaurants)
 
 $(document).on("click", ".like-btn", function(event){
     $(event.target).prop('disabled', true).addClass('disabled');
-    // console.log("LIKE", event.target)
-    // event.target = "Added to favorites!";
-    //favoritesButton.innerHTML = "Added to favorites";
     let restaurantId = $(event.target).attr("id").replace("like", "")
     console.log(restaurantId);
     let restaurant = restaurantList.find(function(rest){
